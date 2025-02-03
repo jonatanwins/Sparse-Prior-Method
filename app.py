424f0bbc02258e46dc281d65cc9184e080107408
Jonatan <jonatankwins@gmail.com>
2025-02-03 15:45:30 +0100

    circular array

--- app.py
+++ app.py
@@ -10,17 +10,28 @@ def initialize_array(array_size, microphone_spacing):
     microphone_positions = np.linspace(-(array_size) / 2, (array_size) / 2, array_size) * microphone_spacing
     return microphone_positions
 
-def plot_array_and_source(array_size=8, source_distance=2.0, source_angle=(1/4)*np.pi, microphone_spacing=0.2):
+def initialize_circular_array(array_size, radius):
+    angle_spacing = 2 * np.pi / array_size
 
-    microphone_positions = initialize_array(array_size, microphone_spacing)
+    x_microphone_positions = np.array( [ radius * np.cos(x * angle_spacing) for x in range(array_size)] )
+    y_microphone_positions = np.array( [ radius * np.sin(x * angle_spacing) for x in range(array_size)] )
+    return x_microphone_positions, y_microphone_positions
+
+def plot_array_and_source(array_size=8, source_distance=2.0, source_angle=(1/4)*np.pi, microphone_spacing=0.2, microphone_radius=False):
+
+    if (microphone_radius):
+        x_microphone_positions, y_microphone_positions = initialize_circular_array(array_size, microphone_radius)
+    else:
+        x_microphone_positions = initialize_array(array_size, microphone_spacing)
+        y_microphone_positions = np.zeros_like(x_microphone_positions)
     
     # Source position
     source_x = source_distance * np.sin(source_angle)
     source_y = source_distance * np.cos(source_angle)
 
     # Plot the array and source
-    lim = max(array_size * microphone_spacing / 2, source_distance) * 1.05
-    plt.scatter(microphone_positions, np.zeros_like(microphone_positions), label="Microphones", color=next(colors))
+    lim = max(array_size * microphone_spacing / 2, source_distance) * 1.05 # migth create issues wih circular array
+    plt.scatter(x_microphone_positions, y_microphone_positions, label="Microphones", color=next(colors))
     plt.scatter(source_x, source_y, label=f"Sound Source", color=next(colors))
     plt.scatter(0, 0, label="Origo", color=next(colors))
     plt.axhline(0, color="gray", linestyle="--", linewidth=0.5)
@@ -33,11 +44,11 @@ def plot_array_and_source(array_size=8, source_distance=2.0, source_angle=(1/4)*
     plt.ylim(-lim, lim)
     plt.show()
 
-def calculate_delays(microphone_positions, source_distance, source_angle, frequency):
+def calculate_delays(x_microphone_positions, y_microphone_positions, source_distance, source_angle, frequency):
     source_x = source_distance * np.sin(source_angle)
     source_y = source_distance * np.cos(source_angle)
 
-    distances = np.sqrt((microphone_positions - source_x)**2 + source_y**2)
+    distances = np.sqrt((x_microphone_positions - source_x)**2 + (y_microphone_positions - source_y)**2)
     delays = distances / speed_of_sound #s
     phase_shifts = 2 * np.pi * frequency * delays # angle frequency times respective delay
 
@@ -51,9 +62,15 @@ def simulate_waveforms(phase_shifts, frequency, sampling_rate):
     
     return t, np.array(waveforms)
 
-def plot_waveforms(frequency, sampling_rate=100000, array_size=8, microphone_spacing=0.1, source_distance=2.0, source_angle= (1/4)*np.pi):
-    microphone_positions = initialize_array(array_size, microphone_spacing)
-    phase_shifts, delays = calculate_delays(microphone_positions, source_distance, source_angle, frequency)
+def plot_waveforms(frequency, sampling_rate=100000, array_size=8, microphone_spacing=0.1, microphone_radius=False, source_distance=2.0, source_angle= (1/4)*np.pi):
+    
+    if (microphone_radius):
+        x_microphone_positions, y_microphone_positions = initialize_circular_array(array_size, microphone_radius)
+    else:
+        x_microphone_positions = initialize_array(array_size, microphone_spacing)
+        y_microphone_positions = np.zeros_like(x_microphone_positions)
+
+    phase_shifts, delays = calculate_delays(x_microphone_positions, y_microphone_positions, source_distance, source_angle, frequency)
     time, waveforms = simulate_waveforms(phase_shifts, frequency, sampling_rate)
     
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,12))
@@ -82,7 +99,7 @@ def plot_waveforms(frequency, sampling_rate=100000, array_size=8, microphone_spa
 
 
 if __name__ == "__main__":
-
-    plot_array_and_source(microphone_spacing=0.1, source_angle=1*np.pi/4)
-    plot_waveforms(frequency=100, microphone_spacing=0.1)
+    r = 0.3
+    plot_array_and_source(array_size=8, microphone_spacing=0.1, source_angle=1*np.pi/4, microphone_radius=r)
+    plot_waveforms(frequency=100, microphone_radius=r)
 
